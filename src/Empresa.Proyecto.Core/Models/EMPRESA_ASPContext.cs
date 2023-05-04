4881937e7ef14081201785bed42f17c87f487c26
Jair Roman <romanjair9728@gmail.com>
2023-05-04 17:27:06 -0600

    Realice la conexion a la base de datos de SQL SERVER 2019, modificando el appsettings y el dbcontext. Tambien agregue los paquetes Nugets de EFSQLO, EFTOOLS, y EF, version 6.015

---
+++ src/Empresa.Proyecto.Core/Models/EMPRESA_ASPContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Empresa.Proyecto.Core.Models
+{
+    public partial class EMPRESA_ASPContext : DbContext
+    {
+        public EMPRESA_ASPContext()
+        {
+        }
+
+        public EMPRESA_ASPContext(DbContextOptions<EMPRESA_ASPContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
